daf73267e8ffe14d276c1b6914a3a06998279c05
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-03-01 11:26:25 +0100

    third commit

--- DemoBlazor.Models/Evento.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DemoBlazor.Models;
-
-public class Evento
-{
-    public int Id {get;set;}
-    public DateTime Data { get; set; }
-    public string? Nome { get; set; }
-    public string? Località { get; set; }
-}
--- DemoBlazor.Models/Interfaces/IDatiEventi.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DemoBlazor.Models.Interfaces;
-
-public interface IDatiEventi
-{
-    List<Evento> EstraiEventiPassati();
-    List<Evento> EstraiEventiFuturi();
-}
